49d3959a3fe0f623b34aad2515a18ad9485e6f11
janakitti <janakittiratana@gmail.com>
2021-11-04 20:24:04 -0400

    Adds delay

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,10 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
+function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  await timeout(2000);
   return NextResponse.rewrite("https://janakittis-top-notch-site.webflow.io/");
 }
